2a5ca63dba299ce6991c9e040376e807c7cfc348
George Rossakis <george@MacBook-Pro-2.local>
2018-07-22 19:23:32 -0400

    further construction

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,29 @@
 $('header').delay(250).animate({opacity: 1} , 1000)
-$(".main-content").delay(500).animate({ opacity: 1 }, 1000)
-$('.web-portfolio').delay(750).animate({left: '-15%'}, 1300)
-$('.contact-div').delay(1000).animate({right: '-15%'}, 1400)
+
+$('.web-portfolio').delay(500).animate({left: '-18%'}, 1200)
+$('.contact-div').delay(750).animate({right: '-18%'}, 1200)
+$(".home-pic-div").delay(1500).animate({ opacity: 1 }, 1000)
 
 
-$('#my-pic-div').on('click' , function(){
-    $(".main-content").animate({ opacity: 0 }, 1000)
-    $('.web-portfolio').animate({left: '-30%'}, 1000)
-    $('.contact-div').animate({right: '-30%'}, 1000)
-    setTimeout(function(){ $(".main-content").empty() }, 1000)
-    setTimeout(function(){$('.about-me-div').animate({ opacity: 1 }, 1000)}, 1000)
+$('.home-pic-div').on('click' , function(){
+    $('.back-arrow-div').animate({left: '4%'});
+    $(".home-pic-div").animate({ opacity: 0 }, 1000)
+    $('.web-portfolio').animate({left: '-40%'}, 1000)
+    $('.contact-div').animate({right: '-40%'}, 1000)
+    $(".home-pic-div").delay(1000).animate({ top: '-100%' }, 1000)
+    $('.about-me-div').animate({ top: '5%' })
+    $('.about-me-div').delay(1000).animate({ opacity: 1 }, 1000)
+    $('.back-arrow-div').delay(1500).animate({ opacity: 1 }, 1000)
 });
+
+$('.back-arrow-div').on('click' , function(){
+    $('.about-me-div').animate({ opacity: 0 }, 400);
+    $('.about-me-div').delay(400).animate({ top: '100%' });
+    $('.back-arrow-div').animate({opacity: 0}, 400);
+    $('.back-arrow-div').delay(400).animate({ left: '-4%' });
+
+    $('.web-portfolio').delay(400).animate({left: '-18%'}, 500)
+    $('.contact-div').delay(400).animate({right: '-15%'}, 500)
+    $(".home-pic-div").animate({ top: '5%' })
+    $(".home-pic-div").delay(400).animate({ opacity: 1 }, 500)
+})
