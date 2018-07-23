3ada6669e8b5f605c8ed29d403983906717407b7
George Rossakis <george@MacBook-Pro-2.local>
2018-07-22 21:57:12 -0400

    tweaks

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,29 +1,39 @@
-$('header').delay(250).animate({opacity: 1} , 1000)
+var portfolioDiv = $('.web-portfolio');
+var contactDiv = $('.contact-div');
+var homePicDiv = $('.home-pic-div');
+var backArrowDiv = $('.back-arrow-div');
+var aboutMeDiv = $('.about-me-div');
 
-$('.web-portfolio').delay(500).animate({left: '-18%'}, 1200)
-$('.contact-div').delay(750).animate({right: '-18%'}, 1200)
-$(".home-pic-div").delay(1500).animate({ opacity: 1 }, 1000)
+$('header').delay(250).animate({opacity: 1} , 1000);
+
+portfolioDiv.delay(500).animate({left: '-18%'}, 1200);
+contactDiv.delay(750).animate({right: '-18%'}, 1200);
+homePicDiv.delay(1500).animate({ opacity: 1 }, 1000);
 
 
-$('.home-pic-div').on('click' , function(){
-    $('.back-arrow-div').animate({left: '4%'});
-    $(".home-pic-div").animate({ opacity: 0 }, 1000)
-    $('.web-portfolio').animate({left: '-40%'}, 1000)
-    $('.contact-div').animate({right: '-40%'}, 1000)
-    $(".home-pic-div").delay(1000).animate({ top: '-100%' }, 1000)
-    $('.about-me-div').animate({ top: '5%' })
-    $('.about-me-div').delay(1000).animate({ opacity: 1 }, 1000)
-    $('.back-arrow-div').delay(1500).animate({ opacity: 1 }, 1000)
+homePicDiv.on('click' , function(){
+    aboutMeDiv.removeClass('hidden-element');
+    backArrowDiv.animate({left: '4%'});
+    homePicDiv.animate({ opacity: 0 }, 1000);
+    portfolioDiv.animate({left: '-40%'}, 1000);
+    contactDiv.animate({right: '-40%'}, 1000);
+    homePicDiv.delay(1000).animate({ top: '-100%' }, 1000);
+    aboutMeDiv.animate({ top: '5%' });
+    aboutMeDiv.delay(1000).animate({ opacity: 1 }, 1000);
+    backArrowDiv.delay(1500).animate({ opacity: 1 }, 1000);
+
+    backArrowDiv.on('click' , function(){
+        backArrowDiv.off('click');
+        aboutMeDiv.animate({ opacity: 0 }, 600);
+        aboutMeDiv.delay(600).animate({ top: '100%' });
+        backArrowDiv.animate({opacity: 0}, 600);
+        backArrowDiv.delay(600).animate({ left: '-4%' });
+        portfolioDiv.delay(600).animate({left: '-18%'}, 500);
+        contactDiv.delay(600).animate({right: '-15%'}, 500);
+        homePicDiv.animate({ top: '5%' });
+        homePicDiv.delay(600).animate({ opacity: 1 }, 500);
+
+        setTimeout(function(){aboutMeDiv.addClass('hidden-element')} , 800);
+    });
 });
 
-$('.back-arrow-div').on('click' , function(){
-    $('.about-me-div').animate({ opacity: 0 }, 400);
-    $('.about-me-div').delay(400).animate({ top: '100%' });
-    $('.back-arrow-div').animate({opacity: 0}, 400);
-    $('.back-arrow-div').delay(400).animate({ left: '-4%' });
-
-    $('.web-portfolio').delay(400).animate({left: '-18%'}, 500)
-    $('.contact-div').delay(400).animate({right: '-15%'}, 500)
-    $(".home-pic-div").animate({ top: '5%' })
-    $(".home-pic-div").delay(400).animate({ opacity: 1 }, 500)
-})
